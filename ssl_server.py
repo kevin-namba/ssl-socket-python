bd1e01c362b73d0250d96486a4fcf5f0f532aa4d
kevin55156 <nambayouhei@keio.jp>
2022-11-16 21:51:06 +0900

    edit

--- ssl_server.py
+++ ssl_server.py
@@ -10,22 +10,6 @@ bindsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 bindsocket.bind((URL, PORT))
 bindsocket.listen(5)
 
-def do_something(connstream, data):
-    print('---------------------')
-    print(data)
-    print('---------------------')
-
-def deal_with_client(connstream,newsocket):
-    data = connstream.read().decode()
-    # null data means the client is finished with us
-    while data:
-        if not do_something(connstream, data):
-            # we'll assume do_something returns False
-            # when we're finished with client
-            break
-        newsocket.send(data)
-    # finished with client
-
 while True:
     newsocket, fromaddr = bindsocket.accept()
     connstream = context.wrap_socket(newsocket, server_side=True)
